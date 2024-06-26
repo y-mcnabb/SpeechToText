f89790933c1368961001df5847ce3f07b081d59e
ymcnabb <y.mcnabb@anchormen.nl>
2024-06-26 10:13:11 +0200

    Add stt to the path to see if it solves the deployment problems

--- stt/app/main.py
+++ stt/app/main.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         logger.warning("Unable to load environment variables!")
 
     uvicorn.run(
-        "app.main:app",
+        "stt.app.main:app",
         host="127.0.0.1",
         port=8000,
         reload=False,
